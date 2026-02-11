db5dc0d1f1fdcfea23e12bf7210036fdde4b1474
Rahul0004R <rahul@hirolaits.com>
2026-02-11 12:30:15 +0530

    [Updated] Fix Static File Serving

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { connectDB } from "./config/db.js";
 import seedRoutes from "./routes/seed.routes.js";
 import shopRoutes from "./routes/shop.routes.js";
 import clearRoutes from "./routes/clear.routes.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
 
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
+// API routes
 app.use("/api/seed", seedRoutes);
 app.use("/api/shop", shopRoutes);
 app.use("/api/clear", clearRoutes);
 
-const startServer = async () => {
-  await connectDB();
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-  app.listen(3000, () => {
-    console.log("shop at http://localhost:3000");
-  });
-};
+// Connect DB once
+connectDB();
 
-startServer();
+export default app;
